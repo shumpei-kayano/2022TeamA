345c4057774dfdc4af7d552af00087ec336446bb
saho-920 <sevenstar9@icloud.com>
2022-10-28 10:33:56 +0900

    Ticketsテーブル作成

---
+++ database/migrations/2022_10_28_012316_create_tickets_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTicketsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tickets', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('store_id');
+            $table->integer('user_id');
+            $table->integer('coupon_id');
+            $table->datetime('term_of_use');
+            $table->integer('flg');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tickets');
+    }
+}
