b2262be101fbbba982a30ca74bc4b34c38a516bf
saho-920 <sevenstar9@icloud.com>
2022-10-28 11:50:54 +0900

    reviewテーブル作成

---
+++ database/migrations/2022_10_28_023142_create_reviews_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateReviewsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reviews', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('user_id');
+            $table->date('visited');
+            $table->text('comment');
+            $table->integer('store_id');
+            $table->datetime('posted_date');
+            $table->integer('star');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reviews');
+    }
+}
