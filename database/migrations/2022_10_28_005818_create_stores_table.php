f0446629535eb004712762a85a4bcd5cae3d5281
ninomiya-hibiki <ooi2172054@stu.o-hara.ac.jp>
2022-10-28 10:06:13 +0900

    storesテーブル作成

---
+++ database/migrations/2022_10_28_005818_create_stores_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStoresTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('stores', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('store_name');
+            $table->string('address');
+            $table->string('link');
+            $table->string('tel');
+            $table->text('service');
+            $table->string('picture1');
+            $table->string('picture2');
+            $table->string('picture3');
+            $table->string('picture4');
+            $table->time('bussiness_hours');
+            $table->string('parking');
+            $table->string('area_id');
+            $table->integer('perfecture_id');
+            $table->double('latitude');
+            $table->double('longitude');
+            $table->integer('review_count');
+            $table->integer('star');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('stores');
+    }
+}
