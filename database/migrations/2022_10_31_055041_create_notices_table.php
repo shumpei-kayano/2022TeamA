6c8cfd61a1a23e4c9e4fadd82fbad19ac32744bd
kentaq23 <k20ken02ta@icloud.com>
2022-10-31 14:57:36 +0900

    noticesテーブル作成

---
+++ database/migrations/2022_10_31_055041_create_notices_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateNoticesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('notices', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('user_id');
+            $table->integer('alert_id');
+            $table->datetime('notice');
+            $table->integer('flg');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('notices');
+    }
+}
