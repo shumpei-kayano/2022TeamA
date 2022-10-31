d1c60da6af21dc0cd390e15ceaf16da53a359e35
Tomoyuki-Okuma <bizarre.dept@gmail.com>
2022-10-31 14:20:00 +0900

    resources/viewsに各画面のテンプレートを作成

---
+++ resources/views/review/person.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head lang="ja">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="/css/app.css" rel="stylesheet">
+</head>
+
+<body>
+
+    <nav class="p-gnav">
+        <ul>
+            <li><a href="" class="p-gnav__home">ホーム</a></li>
+            <li><a href="" class="p-gnav__coupon">クーポン</a></li>
+            <li><a href="" class="p-gnav__gacha">ガチャ</a></li>
+            <li><a href="" class="p-gnav__badge">バッジ</a></li>
+            <li><a href="" class="p-gnav__account">アカウント</a></li>
+        </ul>
+    </nav>
+    <div class="c-container">
+        <p>ここにコンテンツが入ります</p>
+    </div>
+</body>
+
+</html>
