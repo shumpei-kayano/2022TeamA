26b95a930e8ee1220b8090278aa838308f1d258f
marine888 <mrnkj.819@gmail.com>
2022-11-01 16:28:22 +0900

    アカウントトップ画面デザイン

--- resources/views/account/index.blade.php
+++ resources/views/account/index.blade.php
@@ -20,7 +20,12 @@
         </ul>
     </nav>
     <div class="c-container">
-        <h1>アカウント</h1>
+        <div class="p-account__top"><p class="p-account__phot"><img src="/images/phot-account.jpg" alt="アバター画像"></p><p class="p-account__nickname">ニックネーム</p></div>
+        <div class="p-account__navgroup"><a class="p-account__nav">投稿したクチコミ</a>
+        <a class="p-account__nav">いいねしたクチコミ</a>
+        <a class="p-account__nav">訪れたスポット</a>
+        <a class="p-account__nav">アカウント設定</a></div>
+        <button type="submit" class="c-btn c-btn--navy">ログアウト</button>
     </div>
 </body>
 
