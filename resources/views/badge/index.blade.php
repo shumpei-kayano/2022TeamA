5a42ab314b4bf6f0d540c798cb7cdab478bff3f9
marine888 <mrnkj.819@gmail.com>
2022-11-02 16:26:05 +0900

    バッジ画面　途中

--- resources/views/badge/index.blade.php
+++ resources/views/badge/index.blade.php
@@ -49,9 +49,139 @@
                     <p class="p-badge__date">2022.11.02</p>
 
                 </div>
+
+            </div>
+        </section>
+
+        <section class="p-badge__section">
+            <h3 class="p-badge__title">訪れた店舗数</h3>
+            <div class="p-badge__group">
+                <div class="p-badge__item">
+                    <p class="p-badge__img">
+                        <img src="/images/badge_3spot_off_10.png" alt="3店舗">
+                    </p>
+
+                    <p class="p-badge__name">3店舗</p>
+                    <p class="p-badge__date">2022.11.02</p>
+
+                </div>
+                <div class="p-badge__item">
+                    <p class="p-badge__img">
+                        <img src="/images/badge_5spot_off_12.png" alt="5店舗">
+                    </p>
+
+                    <p class="p-badge__name">5店舗</p>
+                    <p class="p-badge__date">2022.11.02</p>
+
+                </div>
+                <div class="p-badge__item">
+                    <p class="p-badge__img">
+                        <img src="/images/badge_10spot_off_14.png" alt="10店舗">
+                    </p>
+
+                    <p class="p-badge__name">10店舗</p>
+                    <p class="p-badge__date">2022.11.02</p>
+
+                </div>
+                <div class="p-badge__item">
+                    <p class="p-badge__img">
+                        <img src="/images/badge_50spot_off_16.png" alt="50店舗">
+                    </p>
+
+                    <p class="p-badge__name">50店舗</p>
+                    <p class="p-badge__date">2022.11.02</p>
+
+                </div>
+            </div>
+        </section>
+
+
+        <section class="p-badge__section">
+            <h3 class="p-badge__title">クチコミ投稿回数</h3>
+            <div class="p-badge__group">
+                <div class="p-badge__item">
+                    <p class="p-badge__img">
+                        <img src="/images/badge_1review_off_18.png" alt="初めての投稿">
+                    </p>
+
+                    <p class="p-badge__name">初めての投稿</p>
+                    <p class="p-badge__date">2022.11.02</p>
+
+                </div>
+                <div class="p-badge__item">
+                    <p class="p-badge__img">
+                        <img src="/images/badge_5review_off_20.png" alt="5回目の投稿">
+                    </p>
+
+                    <p class="p-badge__name">5回目の投稿</p>
+                    <p class="p-badge__date">2022.11.02</p>
+
+                </div>
+                <div class="p-badge__item">
+                    <p class="p-badge__img">
+                        <img src="/images/badge_10review_off_22.png" alt="10回目の投稿">
+                    </p>
+
+                    <p class="p-badge__name">10回目の投稿</p>
+                    <p class="p-badge__date">2022.11.02</p>
+
+                </div>
+                <div class="p-badge__item">
+                    <p class="p-badge__img">
+                        <img src="/images/badge_50review_off_24.png" alt="50回目の投稿">
+                    </p>
+
+                    <p class="p-badge__name">50回目の投稿</p>
+                    <p class="p-badge__date">2022.11.02</p>
+
+                </div>
+            </div>
+        </section>
+
+        <section class="p-badge__section">
+            <h3 class="p-badge__title">獲得したいいね数</h3>
+            <div class="p-badge__group">
+                <div class="p-badge__item">
+                    <p class="p-badge__img">
+                        <img src="/images/badge_1good_off_26.png" alt="1いいね獲得">
+                    </p>
+
+                    <p class="p-badge__name">1いいね獲得</p>
+                    <p class="p-badge__date">2022.11.02</p>
+
+                </div>
+                <div class="p-badge__item">
+                    <p class="p-badge__img">
+                        <img src="/images/badge_20good_off_28.png" alt="20いいね獲得">
+                    </p>
+
+                    <p class="p-badge__name">20いいね獲得</p>
+                    <p class="p-badge__date">2022.11.02</p>
+
+                </div>
+                <div class="p-badge__item">
+                    <p class="p-badge__img">
+                        <img src="/images/badge_50good_off_30.png" alt="50いいね獲得">
+                    </p>
+
+                    <p class="p-badge__name">50いいね獲得</p>
+                    <p class="p-badge__date">2022.11.02</p>
+
+                </div>
+                <div class="p-badge__item">
+                    <p class="p-badge__img">
+                        <img src="/images/badge_100good_off_32.png" alt="100いいね獲得">
+                    </p>
+
+                    <p class="p-badge__name">100いいね獲得</p>
+                    <p class="p-badge__date">2022.11.02</p>
+
+                </div>
             </div>
         </section>
     </div>
+
+
     <nav class="p-gnav">
         <ul>
             <li><a href="" class="p-gnav__home">ホーム</a></li>
