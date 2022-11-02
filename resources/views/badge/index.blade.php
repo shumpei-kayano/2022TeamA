7070d7c48ca694d054b32f8f528448ad38e9bd63
marine888 <mrnkj.819@gmail.com>
2022-11-02 15:18:34 +0900

    バッジ　途中

--- resources/views/badge/index.blade.php
+++ resources/views/badge/index.blade.php
@@ -9,7 +9,28 @@
 </head>
 
 <body>
-
+    <div class="c-container">
+    <div class="p-badge__section">
+<section>
+    <div class="p-badge__title"><h3>ガチャを回した回数</h3></div>
+    <div class="p-badge__group">
+    <div class ="p-badge__item">
+    <div class="p-badge__img">
+    <img src="/images/badge_5gacha_off_02.png" alt="5ガチャ">
+    <img src="/images/badge_20gacha_off_04.png" alt="5ガチャ">
+    <img src="/images/badge_50gacha_off_06.png" alt="5ガチャ">
+    <img src="/images/badge_100gacha_off_08.png" alt="5ガチャ">
+    <div class="p-badge__name">
+    <p>5ガチャ</p>
+    <div class="p-badge__date">
+{{--  日付はデータベースから取ってくる  --}}
+    </div>
+    </div>
+    </div>
+    </div>
+    </div>
+</section>
+    </div>
     <nav class="p-gnav">
         <ul>
             <li><a href="" class="p-gnav__home">ホーム</a></li>
@@ -19,8 +40,7 @@
             <li><a href="" class="p-gnav__account">アカウント</a></li>
         </ul>
     </nav>
-    <div class="c-container">
-        <p>バッジ</p>
+
     </div>
 </body>
 
