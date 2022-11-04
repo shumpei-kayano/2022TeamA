5db252e8f967680aec7958ce43a9b030fc78b141
shin0823 <shinbay0823@gmail.com>
2022-11-04 11:46:24 +0900

    ガチャエラー画面途中

--- resources/views/gacha/index.blade.php
+++ resources/views/gacha/index.blade.php
@@ -14,7 +14,7 @@
         <ul>
             <li><a href="" class="p-gnav__home">ホーム</a></li>
             <li><a href="" class="p-gnav__coupon">クーポン</a></li>
-              <li><a href="" class="p-gnav__gacha">ガチャ</a></li>
+            <li><a href="" class="p-gnav__gacha">ガチャ</a></li>
             <li><a href="" class="p-gnav__badge">バッジ</a></li>
             <li><a href="" class="p-gnav__account">アカウント</a></li>
         </ul>
