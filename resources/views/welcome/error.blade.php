a62457d2eb3ffd499a395df5119bf391cb71b23b
shin0823 <shinbay0823@gmail.com>
2022-11-04 14:00:03 +0900

    ☆ログインエラー完成

---
+++ resources/views/welcome/error.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head lang="ja">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="/css/app.css" rel="stylesheet">
+</head>
+
+<body>
+    <div class="c-container">
+        <p class="p-welcome__logo"><img src="/images/logo-plat.png" alt="plat"></p>
+        <form action="" class="c-form">
+            <div class="c-form__group">
+                <label for="">ニックネーム</label><input type="text">
+            </div>
+
+            <div class="c-form__group">
+                <label for="">パスワード</label><input type="password">
+            </div>
+
+            <button type="submit" class="c-btn c-btn--pink c-btn--50p">ログイン</button>
+
+            <p class="p-welcome__sineup"><a href="">新規会員登録</a></p>
+
+
+
+        </form>
+    </div>
+    <div class="c-modal">
+        <div class="c-modal__box">
+            <p class="c-modal__title">ログインエラー</p>
+            <p class="c-modal__text">ニックネーム、またはパスワードが間違っています。ご確認の上、再度お試し下さい</p>
+            <button type="submit" class="c-btn c-btn--navy">OK</button>
+        </div>
+    </div>
+</body>
+
+</html>
