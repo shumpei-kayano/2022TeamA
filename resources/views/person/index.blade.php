3d808cc23e1c66a5ea6a4c4a914e1c16ef7039ef
kentaq23 <k20ken02ta@icloud.com>
2022-11-04 14:59:10 +0900

    クチコミ関係　途中

--- resources/views/person/index.blade.php
+++ resources/views/person/index.blade.php
@@ -20,7 +20,53 @@
         </ul>
     </nav>
     <div class="c-container">
-       <h1>ホーム</h1>
+
+
+        <div class="c-hukidashi">
+            <p class="c-hukidashi__username"> ニックネーム</p>
+            <div class="c-hukidashi__flem">
+                <div class="c-hukidashi__header">
+                    <h3 class="c-hukidashi__tittle">店舗名</h3>
+                    <div class="c-hukidashi__star">★★★☆☆</div>
+                </div>
+                <div class="c-hukidashi__honbun">
+                    職人さんが経営する個人オーナーのイタリアンではなかなかできない演出力である。その反面、こういった資本力のある会社さんのお店だとクオリティが・・・
+                </div>
+            </div>
+        </div>
+        <div class="c-hukidashi__date">
+            2022/10/29
+        </div>
+        <div class="c-hukidashi--other">
+            <p class="c-hukidashi__username"> ニックネーム</p>
+            <div class="c-hukisashi__flem">
+                <div class="c-hukidashi__header">
+                    <h3 class="c-hukidashi__tittle">店舗名</h3>
+                    <div class="c-hukidashi__star">★★★☆☆</div>
+                </div>
+                <div class="c-hukidashi__honbun">
+                    職人さんが経営する個人オーナーのイタリアンではなかなかできない演出力である。その反面、こういった資本力のある会社さんのお店だとクオリティが・・・
+                </div>
+                <div class="c-hukidashi__good">
+                    <div class="c-hukidashi__good__icon">
+                        <img src="/images/good-icon.png" alt="いいねアイコン">
+                    </div>
+                    <div class="c-hukidashi__goodnumber">
+                        50
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="c-hukidashi__edit">
+            <div class="c-hukidashi__edit_icon">
+                <img src="/images/pencil.png">編集
+            </div>
+            <div class="c-hukidashi__delete_icon">
+                <img src="/images/gomibako.png">削除
+            </div>
+        </div>
+
+
     </div>
 </body>
 
