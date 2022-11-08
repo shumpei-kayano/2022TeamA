046d2a003059478faa0c9e6dfb37acde9b823c66
ninomiya-hibiki <ooi2172054@stu.o-hara.ac.jp>
2022-11-08 10:50:57 +0900

    ☆クーポン使用確認

---
+++ resources/views/coupon/confirmation.blade.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head lang="ja">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="/css/app.css" rel="stylesheet">
+</head>
+
+<body>
+
+    <nav class="p-gnav">
+        <ul>
+            <li><a href="" class="p-gnav__home">ホーム</a></li>
+            <li><a href="" class="p-gnav__coupon">クーポン</a></li>
+            <li><a href="" class="p-gnav__gacha">ガチャ</a></li>
+            <li><a href="" class="p-gnav__badge">バッジ</a></li>
+            <li><a href="" class="p-gnav__account">アカウント</a></li>
+        </ul>
+    </nav>
+    <div class="c-container">
+        <div class="c-coupon">
+            <div class="c-btn--tab">
+                <a class="is-active" href="">使用可能</a>
+                <a href="">使用済み</a>
+            </div>
+            <div class="c-coupon__box">
+                <p class="c-coupon__use">有効期限：2022.10.30</p>
+                <div class="c-modal__flex">
+                    <p class="c-modal__flex__img">
+                        <img src="/images/coupon.jpg" alt="クーポン">
+                    </p>
+                    <div class="c-modal__flex__text">
+                        <p class="c-modal__flex__coupon">ハロウィン限定アフタヌーンティー50%OFF</p>
+                        <p class="c-modal__flex__store">the LOUNGE</p>
+                    </div>
+                </div>
+                <div class="c-coupon__a">
+                    <button type="submit" class="c-coupon__a__btn">詳細を見る</button>
+                </div>
+                <p class="c-coupon__address">大分市別府市鉄輪499-18</p>
+                <button type="submit" class="c-btn c-btn--navy">このクーポンを使う</button>
+            </div>
+        </div>
+
+        <div class="c-coupon__box">
+            <p class="c-coupon__use">有効期限：2022.11.20</p>
+            <div class="c-modal__flex">
+                <p class="c-modal__flex__img"><img src="/images/Beer.jpg" alt="生ビール"></p>
+                <div class="c-modal__flex__text">
+                    <p class="c-modal__flex__coupon">生ビール1杯無料</p>
+                    <p class="c-modal__flex__store">ROUTE 502 COFFEE</p>
+                </div>
+            </div>
+            <div class="c-coupon__a">
+                <button type="submit" class="c-coupon__a__btn">詳細を見る</button>
+            </div>
+            <p class="c-coupon__address">大分県臼杵市市浜1129-3</p>
+            <button type="submit" class="c-btn c-btn--navy">このクーポンを使う</button>
+        </div>
+    </div>
+    <div class="c-modal">
+        <div class="c-modal__box">
+            <p class="c-modal__top">本当に使用しますか</p>
+            <p class="c-modal__use">このクーポンは一回のみ使用できます。</p>
+            <div class="c-modal__flex">
+                <p class="c-modal__flex__img"><img src="/images/coupon.jpg" alt="獲得クーポン"></p>
+                <div class="c-modal__flex__text">
+                    <p class="c-modal__flex__coupon">ハロウィン限定アフタヌーンティー50%OFF</p>
+                    <p class="c-modal__flex__store">the LOUNGE</p>
+                </div>
+            </div>
+            <button type="submit" class="c-btn c-btn--navy">クーポンを使う</button>
+            <p class="c-modal__sineup"><a href="">後で</a></p>
+        </div>
+    </div>
+</body>
+
+</html>
