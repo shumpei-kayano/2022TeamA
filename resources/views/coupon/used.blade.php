debd58814e71b8ace40690d75a3381192bdc9e8b
ninomiya-hibiki <ooi2172054@stu.o-hara.ac.jp>
2022-11-08 15:50:16 +0900

    ☆クーポン使用済み画面完成

--- resources/views/coupon/used.blade.php
+++ resources/views/coupon/used.blade.php
@@ -39,6 +39,48 @@
                     </div>
                 </div>
             </div>
+            <div class="c-coupon__box">
+                <div class="c-coupon__top">
+                    <p class="c-coupon__use">2022.7.15に使用済み</p>
+                    <a class="c-btn c-btn--navy is-inactive c-btn--small">クチコミを書く</a>
+                </div>
+                <div class="c-modal__flex">
+                    <p class="c-modal__flex__img"><img src="/images/coupon.jpg" alt="クーポン"></p>
+                    <div class="c-modal__flex__text">
+                        <p class="c-modal__flex__coupon">ハロウィン限定アフタヌーンティー50%OFF</p>
+                        <p class="c-modal__flex__store">the LOUNGE</p>
+                        <p class="c-modal__flex__address">大分市別府市鉄輪499-18</p>
+                    </div>
+                </div>
+            </div>
+            <div class="c-coupon__box">
+                <div class="c-coupon__top">
+                    <p class="c-coupon__use">2022.7.15に使用済み</p>
+                    <a class="c-btn c-btn--navy is-inactive c-btn--small">クチコミを書く</a>
+                </div>
+                <div class="c-modal__flex">
+                    <p class="c-modal__flex__img"><img src="/images/coupon.jpg" alt="クーポン"></p>
+                    <div class="c-modal__flex__text">
+                        <p class="c-modal__flex__coupon">ハロウィン限定アフタヌーンティー50%OFF</p>
+                        <p class="c-modal__flex__store">the LOUNGE</p>
+                        <p class="c-modal__flex__address">大分市別府市鉄輪499-18</p>
+                    </div>
+                </div>
+            </div>
+            <div class="c-coupon__box">
+                <div class="c-coupon__top">
+                    <p class="c-coupon__use">2022.7.15に使用済み</p>
+                    <a class="c-btn c-btn--navy c-btn--small">クチコミを書く</a>
+                </div>
+                <div class="c-modal__flex">
+                    <p class="c-modal__flex__img"><img src="/images/Beer.jpg" alt="生ビール"></p>
+                    <div class="c-modal__flex__text">
+                        <p class="c-modal__flex__coupon">生ビール1杯無料</p>
+                        <p class="c-modal__flex__store">ROUTE 502 COFFEE</p>
+                        <p class="c-modal__flex__address">大分県臼杵市市浜1129-3</p>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </body>
