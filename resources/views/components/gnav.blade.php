3ac87f1866df251266e44bc681a334991a40ce97
Tomoyuki-Okuma <bizarre.dept@gmail.com>
2022-11-10 10:03:20 +0900

    グローバルナビをコンポーネント化

---
+++ resources/views/components/gnav.blade.php
@@ -0,0 +1,9 @@
+  <nav class="p-gnav">
+      <ul>
+          <li><a href="" class="p-gnav__home">ホーム</a></li>
+          <li><a href="" class="p-gnav__coupon">クーポン</a></li>
+          <li><a href="" class="p-gnav__gacha">ガチャ</a></li>
+          <li><a href="" class="p-gnav__badge">バッジ</a></li>
+          <li><a href="" class="p-gnav__account">アカウント</a></li>
+      </ul>
+  </nav>
