5cbc88394320f4611d90828c0805a0754efe3be7
kentaq23 <k20ken02ta@icloud.com>
2022-11-10 12:00:02 +0900

    ☆訪れたスポット完成

--- resources/views/tourist/index.blade.php
+++ resources/views/tourist/index.blade.php
@@ -13,8 +13,62 @@
     @component('components.gnav')
     @endcomponent
     <div class="c-container">
-        <h1>各店舗</h1>
+        <div class="c-header">
+            <a href="" class="c-back">戻る</a>
+            <h4>訪れたスポット</h4>
+        </div>
+        <p class="c-hukidashi__date--spot">
+            2022/10/29
+        </p>
+        <div class="c-coupon__box--spot">
+            <div class="c-modal__flex">
+                <p class="c-modal__flex__img"><img src="/images/coupon.jpg" alt="クーポン"></p>
+                <div class="c-modal__flex__text">
+                    <p class="c-modal__flex__coupon">cafe Green Brown</p>
+                    <p class="c-modal__flex__store">大分市別府市鉄輪499-18</p>
+                    <p class="c-modal__flex__address">
+                        白いスタイリッシュな建物が目印のカフェ。メニューのほとんどを陶芸作家が手掛あああああああああああああああああああああああああああああああああああ</p>
+                </div>
+            </div>
+        </div>
+        <div class="c-coupon__box">
+            <div class="c-modal__flex">
+                <p class="c-modal__flex__img"><img src="/images/lunch.png" alt="クーポン"></p>
+                <div class="c-modal__flex__text">
+                    <p class="c-modal__flex__coupon">つばめ食堂</p>
+                    <p class="c-modal__flex__store">大分県大分市玉沢335-3</p>
+                    <p class="c-modal__flex__address">
+                        ここでいただけるのは、地元の無農薬野菜・有機野菜をたっぷり使ったプレートラあああああああああああああああああああああああああああああああああああああああああああ</p>
+                </div>
+            </div>
+        </div>
+        <p class="c-hukidashi__date--spot">
+            2022/10/29
+        </p>
+        <div class="c-coupon__box--spot">
+            <div class="c-modal__flex">
+                <p class="c-modal__flex__img"><img src="/images/coupon.jpg" alt="クーポン"></p>
+                <div class="c-modal__flex__text">
+                    <p class="c-modal__flex__coupon">cafe Green Brown</p>
+                    <p class="c-modal__flex__store">大分市別府市鉄輪499-18</p>
+                    <p class="c-modal__flex__address">
+                        白いスタイリッシュな建物が目印のカフェ。メニューのほとんどを陶芸作家が手掛あああああああああああああああああああああああああああああああああああ</p>
+                </div>
+            </div>
+        </div>
+        <div class="c-coupon__box">
+            <div class="c-modal__flex">
+                <p class="c-modal__flex__img"><img src="/images/lunch.png" alt="クーポン"></p>
+                <div class="c-modal__flex__text">
+                    <p class="c-modal__flex__coupon">つばめ食堂</p>
+                    <p class="c-modal__flex__store">大分県大分市玉沢335-3</p>
+                    <p class="c-modal__flex__address">
+                        ここでいただけるのは、地元の無農薬野菜・有機野菜をたっぷり使ったプレートラあああああああああああああああああああああああああああああああああああああああああああ</p>
+                </div>
+            </div>
+        </div>
     </div>
+
 </body>
 
 </html>
