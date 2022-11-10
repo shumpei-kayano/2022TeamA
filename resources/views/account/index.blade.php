a4f7e4d42ef1afa78e004adbb12f2c54243b493d
Tomoyuki-Okuma <bizarre.dept@gmail.com>
2022-11-10 10:05:15 +0900

    全てのgnavをコンポーネントに変更

--- resources/views/account/index.blade.php
+++ resources/views/account/index.blade.php
@@ -10,21 +10,18 @@
 
 <body>
 
-    <nav class="p-gnav">
-        <ul>
-            <li><a href="" class="p-gnav__home">ホーム</a></li>
-            <li><a href="" class="p-gnav__coupon">クーポン</a></li>
-            <li><a href="" class="p-gnav__gacha">ガチャ</a></li>
-            <li><a href="" class="p-gnav__badge">バッジ</a></li>
-            <li><a href="" class="p-gnav__account">アカウント</a></li>
-        </ul>
-    </nav>
+    @component('components.gnav')
+    @endcomponent
     <div class="c-container">
-        <div class="p-account__top"><p class="p-account__phot"><img src="/images/phot-account.jpg" alt="アバター画像"></p><p class="p-account__nickname">ニックネーム</p></div>
+        <div class="p-account__top">
+            <p class="p-account__phot"><img src="/images/phot-account.jpg" alt="アバター画像"></p>
+            <p class="p-account__nickname">ニックネーム</p>
+        </div>
         <div class="p-account__navgroup"><a class="p-account__nav">投稿したクチコミ</a>
-        <a class="p-account__nav">いいねしたクチコミ</a>
-        <a class="p-account__nav">訪れたスポット</a>
-        <a class="p-account__nav">アカウント設定</a></div>
+            <a class="p-account__nav">いいねしたクチコミ</a>
+            <a class="p-account__nav">訪れたスポット</a>
+            <a class="p-account__nav">アカウント設定</a>
+        </div>
         <button type="submit" class="c-btn c-btn--navy">ログアウト</button>
     </div>
 </body>
