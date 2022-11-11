fe1ead629c900d07ceaf80403011627be6665efb
ninomiya-hibiki <ooi2172054@stu.o-hara.ac.jp>
2022-11-11 11:01:52 +0900

    adminコントローラーfunction記述

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -6,5 +6,44 @@ use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
-    //
+    public function watch(){
+
+    }
+    public function in(){
+        
+    }
+    public function enter(){
+        
+    }
+    public function show(){
+        
+    }
+    public function edit(){
+        
+    }
+    public function update(){
+        
+    }
+    public function look(){
+        
+    }
+    public function see(){
+        
+    }
+    public function rewrite(){
+        
+    }
+    public function set(){
+        
+    }
+    public function add(){
+        
+    }
+    public function create(){
+        
+    }
+    public function view(){
+        
+    }
+
 }
