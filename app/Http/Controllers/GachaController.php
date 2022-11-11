f6f4c531487e9d15d6eea9e186f37a7dcec62790
marine888 <mrnkj.819@gmail.com>
2022-11-11 11:01:38 +0900

    gachacontroller アクション　記述

--- app/Http/Controllers/GachaController.php
+++ app/Http/Controllers/GachaController.php
@@ -6,5 +6,19 @@ use Illuminate\Http\Request;
 
 class GachaController extends Controller
 {
-    //
+    public function see(){
+
+    }
+
+    public function play(){
+
+    }
+
+    public function stag(){
+
+    }
+
+    public function get(){
+        
+    }
 }
