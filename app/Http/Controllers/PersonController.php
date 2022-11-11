d26e9d94013626a8cf2e67c69f705237e9df1312
ninomiya-hibiki <ooi2172054@stu.o-hara.ac.jp>
2022-11-11 11:05:45 +0900

    PersonControllerアクション記述

--- app/Http/Controllers/PersonController.php
+++ app/Http/Controllers/PersonController.php
@@ -6,5 +6,26 @@ use Illuminate\Http\Request;
 
 class PersonController extends Controller
 {
-    //
+    public function out(){
+
+    }
+    public function add(){
+        
+    }
+    public function create(){
+        
+    }
+    public function home(){
+        
+    }
+    public function good(){
+        
+    }
+    public function show(){
+        
+    }
+    public function limit(){
+        
+    }
+
 }
