d794d0329984347ff337fd162dbe62e80916725a
shin0823 <shinbay0823@gmail.com>
2022-11-11 11:20:03 +0900

    AccountController return view記述

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -7,25 +7,25 @@ use Illuminate\Http\Request;
 class AccountController extends Controller
 {
     public function set(){
-
+        return view('account/set');
     }
     public function spot(){
-
+        return view('account/spot');
     }
     public function update(){
-
+        return view('account/update');
     }
     public function delete(){
-
+        return view('account/delete');
     }
     public function remove(){
-
+        return view('account/remove');
     }
     public function edit(){
-
+        return view('account/edit');
     }
     public function show(){
-
+        return view('account/show');
     }
     public function good(){
 
