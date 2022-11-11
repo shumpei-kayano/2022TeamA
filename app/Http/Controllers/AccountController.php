5215d59d867b1d5e496becddc35bbb336f22c052
shin0823 <shinbay0823@gmail.com>
2022-11-11 10:59:50 +0900

    AccountController アクション 記述

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -6,5 +6,28 @@ use Illuminate\Http\Request;
 
 class AccountController extends Controller
 {
-    //
+    public function set(){
+
+    }
+    public function spot(){
+
+    }
+    public function update(){
+
+    }
+    public function delete(){
+
+    }
+    public function remove(){
+
+    }
+    public function edit(){
+
+    }
+    public function show(){
+
+    }
+    public function good(){
+
+    }
 }
