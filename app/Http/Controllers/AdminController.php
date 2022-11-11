9308c008e1e2eec476d0975eba21689f21884a1e
marine888 <mrnkj.819@gmail.com>
2022-11-11 11:27:01 +0900

    admincontroller return view 記述

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -7,43 +7,43 @@ use Illuminate\Http\Request;
 class AdminController extends Controller
 {
     public function watch(){
-
+        return view('admin/watch');
     }
     public function in(){
-        
+        return view('admin/in');
     }
     public function enter(){
         
     }
     public function show(){
-        
+        return view('admin/show');
     }
     public function edit(){
-        
+        return view('admin/edit');
     }
     public function update(){
         
     }
     public function look(){
-        
+        return view('admin/look');
     }
     public function see(){
-        
+        return view('admin/see');
     }
     public function rewrite(){
-        
+        return view('admin/rewrite');
     }
     public function set(){
         
     }
     public function add(){
-        
+        return view('admin/add');
     }
     public function create(){
         
     }
     public function view(){
-        
+        return view('admin/view');
     }
 
 }
