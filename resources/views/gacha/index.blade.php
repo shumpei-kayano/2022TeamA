417f4ec3a7d5359475b705774be52f57b9ab2513
shin0823 <shinbay0823@gmail.com>
2022-11-14 11:40:13 +0900

    google map

--- resources/views/gacha/index.blade.php
+++ resources/views/gacha/index.blade.php
@@ -15,28 +15,29 @@
     <div class="c-container u-padding-top--0">
         <a id="btn-open">制限回数オーバー</a>
         <div class="p-gacha-top">
-            <div class="p-gacha__map"><iframe
+            <div id="googleMap" class="p-gacha__map">
+                {{--  <iframe
                     src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d6674.726977339957!2d131.6073871096625!3d33.23077951304794!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sja!2sjp!4v1667368193255!5m2!1sja!2sjp"
                     width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"
-                    referrerpolicy="no-referrer-when-downgrade"></iframe>
-                <div class="p-gacha__handle p-gacha__handle--slideup">
-                    <p class="p-gacha__circle"><img src="/images/gacha-circle.png" alt="ガチャワッカ"></p>
-                    <p class="p-gacha__mawasu"><a href="{{ route('gacha/staging') }}"><img src="/images/turn.png"
-                                alt="ガチャ回す"></a>
+                    referrerpolicy="no-referrer-when-downgrade"></iframe>  --}}</div>
+            <div class="p-gacha__handle p-gacha__handle--slideup">
+                <p class="p-gacha__circle"><img src="/images/gacha-circle.png" alt="ガチャワッカ"></p>
+                <p class="p-gacha__mawasu"><a href="{{ route('gacha/staging') }}"><img src="/images/turn.png"
+                            alt="ガチャ回す"></a>
 
-                    </p>
-                    @component('components.modal')
-                        @slot('title')
-                            <p class="c-modal__title">制限回数オーバー</p>
-                        @endslot
-                        @slot('content')
-                            このエリアで回すことができるガチャの制限数を越えました。他のエリアでお試し下さい。
-                        @endslot
-                        @slot('button')
-                            <button type="submit" class="c-btn c-btn--navy u-margin-top--0">OK</button>
-                        @endslot
-                    @endcomponent
-                </div>
+                </p>
+                @component('components.modal')
+                    @slot('title')
+                        <p class="c-modal__title">制限回数オーバー</p>
+                    @endslot
+                    @slot('content')
+                        このエリアで回すことができるガチャの制限数を越えました。他のエリアでお試し下さい。
+                    @endslot
+                    @slot('button')
+                        <button type="submit" class="c-btn c-btn--navy u-margin-top--0">OK</button>
+                    @endslot
+                @endcomponent
+
             </div>
         </div>
     </div>
@@ -50,6 +51,40 @@
         dialog.querySelector('.c-btn').addEventListener('click', () => {
             dialog.close();
         });
+        // ボタンを押した時の処理
+        window.onload = function() {
+            // 位置情報を取得する
+            navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
+        };
+    </script>
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAtYsX-DTTQHaRPfZ3xTaCrtPoKVv2k6nM&callback=initMap" async
+        defer></script>
+    <script>
+        function initMap() {
+            function success(pos) {
+                var lat = pos.coords.latitude;
+                var lng = pos.coords.longitude;
+                var latlng = new google.maps.LatLng(lat, lng); //中心の緯度, 経度
+                var map = new google.maps.Map(document.getElementById('googleMap'), {
+                    zoom: 17,
+                    center: latlng
+                });
+                var marker = new google.maps.Marker({
+                    position: latlng, //マーカーの位置（必須）
+                    map: map //マーカーを表示する地図
+                });
+            }
+
+            function fail(error) {
+                alert('位置情報の取得に失敗しました。エラーコード：' + error.code);
+                var latlng = new google.maps.LatLng(35.6812405, 139.7649361); //東京駅
+                var map = new google.maps.Map(document.getElementById('maps'), {
+                    zoom: 10,
+                    center: latlng
+                });
+            }
+            navigator.geolocation.getCurrentPosition(success, fail);
+        }
     </script>
 </body>
 
