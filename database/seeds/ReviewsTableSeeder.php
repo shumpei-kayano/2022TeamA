5631ffb64094b2d208d2a571352f99a3e3f3e9e7
saho-920 <sevenstar9@icloud.com>
2022-11-16 12:15:29 +0900

    ホーム画面　途中

---
+++ database/seeds/ReviewsTableSeeder.php
@@ -0,0 +1,58 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class ReviewsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $param=[
+            'user_id'=>'1',
+            'visited'=>'2022/10/31',
+            'comment'=>'職人さんが経営する個人オーナーではなかなかできない演出力である。
+            その反面、こういった資本力のある会社さんのお店だとクオリティがすごかった。',
+            'store_id'=>'4',
+            'posted_date'=>'2022/11/01',
+            'star'=>'3'
+        ];
+        DB::table('reviews')->insert($param);
+
+        $param=[
+            'user_id'=>'3',
+            'visited'=>'2022/11/02',
+            'comment'=>'今朝、モーニングのチーズとトマトを頼んだ。写真がとても美味しそうだったからだ。
+            トマトも分厚く食べごたえがありそうで、具が中から溢れんばかりの美味しいをかもしだしていた。',
+            'store_id'=>'11',
+            'posted_date'=>'2022/11/10',
+            'star'=>'3'
+        ];
+        DB::table('reviews')->insert($param);
+
+        $param=[
+            'user_id'=>'2',
+            'visited'=>'2022/10/15',
+            'comment'=>'黒胡麻アイスクリームは胡麻の味がしっかり！どちらかと言うと甘めです。
+            黒胡麻餡はさらに胡麻が強く、あんこらしいねっとりとした舌触りと甘さが冷たいアイスやホイップと相性抜群です(*^^*)',
+            'store_id'=>'7',
+            'posted_date'=>'2022/10/18',
+            'star'=>'4'
+        ];
+        DB::table('reviews')->insert($param);
+        $param=[
+            'user_id'=>'2',
+            'visited'=>'2022/10/20',
+            'comment'=>'別府温泉街の中心、いでゆ坂沿いに在る別府温泉ならではの地熱で蒸した料理を提供する店。
+            地獄めぐりをしている観光客に人気の料理店です。',
+            'store_id'=>'15',
+            'posted_date'=>'2022/11/16',
+            'star'=>'3'
+        ];
+        DB::table('reviews')->insert($param);
+
+    }
+}
--- resources/views/person/index.blade.php
+++ resources/views/person/index.blade.php
@@ -21,35 +21,35 @@
                 </div>
             </div>
         </a>
-        <p class="c-hukidashi__date">
-            2022/10/29
-        </p>
-        <div class="c-hukidashi c-hukidashi--a">
-            <p class="c-hukidashi__photo">
-                <img src="/images/phot-account.jpg">
+        @foreach ($reviews as $review)
+            <p class="c-hukidashi__date">
+                {{ $review->posted_date }}
             </p>
-            <div class="c-fukidashi__container">
-                <p class="c-hukidashi__username"> ニックネーム</p>
-                <div class="c-hukidashi__frame">
-                    <div class="c-hukidashi__header">
-                        <h3 class="c-hukidashi__tittle">店舗名</h3>
-                        <div class="c-hukidashi__star--a">
-                            <div class="p-admin__star--spot">
-                                <img src="/images/star.png" alt="">
-                                <img src="/images/star.png" alt="">
-                                <img src="/images/star.png" alt="">
-                                <img src="/images/star.black.png" alt="">
-                                <img src="/images/star.black.png" alt="">
+            <div class="c-hukidashi c-hukidashi--a">
+                <p class="c-hukidashi__photo">
+                    <img src={{ $review->user->icon_photo }}>
+                </p>
+                <div class="c-fukidashi__container">
+                    <p class="c-hukidashi__username">{{ $review->user->name }}</p>
+                    <div class="c-hukidashi__frame">
+                        <div class="c-hukidashi__header">
+                            <h3 class="c-hukidashi__tittle"> {{ $review->store->store_name }}</h3>
+                            <div class="c-hukidashi__star--a">
+                                <div class="p-admin__star--spot">
+                                    <img src="/images/star.png" alt="">
+                                    <img src="/images/star.png" alt="">
+                                    <img src="/images/star.png" alt="">
+                                    <img src="/images/star.black.png" alt="">
+                                    <img src="/images/star.black.png" alt="">
+                                </div>
                             </div>
                         </div>
+                        <p class="c-hukidashi__honbun">{{ $review->comment }}</p>
+                        {{--  <p class="c-hukidashi__visited">訪問日{{ $review->visited }}</p>  --}}
                     </div>
-                    <p class="c-hukidashi__honbun">
-                        職人さんが経営する個人オーナーのイタリアンではなかなかできない演出力である。その反面、こういった資本力のある会社さんのお店だとクオリティが・・・
-                    </p>
                 </div>
             </div>
-
-        </div>
+        @endforeach
         <p class="c-hukidashi__date">
             2022/10/29
         </p>
