b42df9dc9d07e41ac9671f92b5279edd758a9e7e
ninomiya-hibiki <ooi2172054@stu.o-hara.ac.jp>
2022-11-16 15:19:45 +0900

    ☆新規登録後の画面 完成

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,34 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
+<head lang="ja">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="/css/app.css" rel="stylesheet">
+</head>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+<div class="c-container">
+    <div class="c-header">
+        <a href="" class="c-back">戻る</a>
+        <h4>新規登録が出来ました！</h4>
+    </div>
+
+    {{--  @section('content')  --}}
+
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="p-add__text">ようこそ！<br>新規登録が完了しました！</div>
+                    <div class="form-group row mb-0">
+                        <div class="col-md-6 offset-md-4">
+                            <button type="submit" class="c-btn c-btn--navy">
+                                {{ __('ホーム画面へ') }}
+                            </button>
                         </div>
-                    @endif
-
-                    You are logged in!
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
-@endsection
