16b13926b11570f3fd1cda4e7fbe591a5206d774
saho-920 <sevenstar9@icloud.com>
2022-11-22 14:13:04 +0900

    ホーム行くようにした

--- resources/views/components/gnav.blade.php
+++ resources/views/components/gnav.blade.php
@@ -1,6 +1,6 @@
   <nav class="p-gnav">
       <ul>
-          <li><a href="{{ route('person/index') }}" class="p-gnav__home">ホーム</a></li>
+          <li><a href="{{ route('person/home') }}" class="p-gnav__home">ホーム</a></li>
           <li><a href="{{ route('coupon/index') }}" class="p-gnav__coupon">クーポン</a></li>
           <li><a href="{{ route('gacha/index') }}" class="p-gnav__gacha">ガチャ</a></li>
           <li><a href="{{ route('badge/index') }}" class="p-gnav__badge">バッジ</a></li>
