1b9957b3e90f669a594fb0316fbdf3b2bc659ab9
Tomoyuki-Okuma <ooi2172404@stu.o-hara.ac.jp>
2023-01-10 13:42:39 +0900

    大原周辺と鉄輪切り替え

--- app/Http/Controllers/GachaController.php
+++ app/Http/Controllers/GachaController.php
@@ -24,7 +24,7 @@ class GachaController extends Controller
         } elseif (false !== strpos($area, '鉄輪')) {
             $ransu = mt_rand(14, 23);
         }
-        
+
 
         $coupons = Coupon::where('id', '=', $ransu)->first();
         return view('gacha.staging', ['coupons' => $coupons]);
@@ -48,12 +48,8 @@ class GachaController extends Controller
         if ($request->session()->has('current_area')) {
             //
         } else {
-            $request->session()->put('current_area', '1');
-
-            $reviews = DB::table('stores')
-            ->where('area_id','=','1')
-            ->count();
-            $request->session()->put('area_count', $reviews);
+            $request->session()->put('current_area', 'oita');
+            $request->session()->put('area_count', '13');
         }
 
         return view('gacha.index');
@@ -61,106 +57,95 @@ class GachaController extends Controller
     public function change_area(Request $request)
     {
         if ($request->has('oita')) {
-            // $request->session()->put('current_area', 'oita');
-            // $request->session()->put('area_count', '13');
-            // $id=Auth::id();
-
-            $request->session()->put('current_area','大原周辺');
-
-            $reviews = DB::table('stores')
-            ->where('area_id','=','1')
-            ->count();
-            $request->session()->put('area_count', $reviews);
+            $request->session()->put('current_area', 'oita');
+            $request->session()->put('area_count', '13');
         } elseif ($request->has('beppu')) {
-            $request->session()->put('current_area', '鉄輪');
-            $reviews = DB::table('stores')
-            ->where('area_id','=','2')
-            ->count();
-            $request->session()->put('area_count',  $reviews);
+            $request->session()->put('current_area', 'beppu');
+            $request->session()->put('area_count', '10');
         }
 
         return redirect('gacha/index');
     }
 
-    public function modeldelete($store_id,$coupon_id)
-{
-    $dt = new \Carbon\Carbon('now','Asia/Tokyo');
-    $id=Auth::id();
-    $ticket=new Ticket;
-    $ticket->store_id=$store_id;
-    $ticket->user_id=$id;
-    $ticket->coupon_id=$coupon_id;
-    $ticket->term_of_use=$dt->addHour(24);
-    $ticket->flg=0;
-    $ticket->save();
+    public function modeldelete($store_id, $coupon_id)
+    {
+        $dt = new \Carbon\Carbon('now', 'Asia/Tokyo');
+        $id = Auth::id();
+        $ticket = new Ticket;
+        $ticket->store_id = $store_id;
+        $ticket->user_id = $id;
+        $ticket->coupon_id = $coupon_id;
+        $ticket->term_of_use = $dt->addHour(24);
+        $ticket->flg = 0;
+        $ticket->save();
 
-    $id=Auth::id();
-    $cond=['user_id' =>$id];
-    $tickets = DB::table('tickets')
-    ->where($cond)
-    ->count();
-    // dd($reviews);
-    // $item=['reviews'=>$reviews];
-    // return redirect()->action('CouponController@used');
+        $id = Auth::id();
+        $cond = ['user_id' => $id];
+        $tickets = DB::table('tickets')
+            ->where($cond)
+            ->count();
+        // dd($reviews);
+        // $item=['reviews'=>$reviews];
+        // return redirect()->action('CouponController@used');
 
-    switch($tickets){
-        case 5:
-         $id=Auth::id();
-        $get=new get;
-        $get->badge_id=1;
-        $get->user_id=$id;
-        $get->get_date=\Carbon\Carbon::today();
-        $get->save();
-        $notice = new Notice;
-        $notice->user_id = $id;
-        $notice->	alert_id = 2;
-        $notice->notice=\Carbon\Carbon::today();
-        $notice->flg=0;
-        $notice->save();
-        break;
-        case 20:
-        $id=Auth::id();
-        $get=new get;
-        $get->badge_id=2;
-        $get->user_id=$id;
-        $get->get_date=\Carbon\Carbon::today();
-        $get->save();
-        $notice = new Notice;
-        $notice->user_id = $id;
-        $notice->	alert_id = 2;
-        $notice->notice=\Carbon\Carbon::today();
-        $notice->flg=0;
-        $notice->save();
-        break;
-        case 50:
-        $id=Auth::id();
-        $get=new get;
-        $get->badge_id=3;
-        $get->user_id=$id;
-        $get->get_date=\Carbon\Carbon::today();
-        $get->save();
-        $notice = new Notice;
-        $notice->user_id = $id;
-        $notice->	alert_id = 2;
-        $notice->notice=\Carbon\Carbon::today();
-        $notice->flg=0;
-        $notice->save();
-        break;
-        case 100:
-        $id=Auth::id();
-        $get=new get;
-        $get->badge_id=4;
-        $get->user_id=$id;
-        $get->get_date=\Carbon\Carbon::today();
-        $get->save();
-        $notice = new Notice;
-        $notice->user_id = $id;
-        $notice->	alert_id = 2;
-        $notice->notice=\Carbon\Carbon::today();
-        $notice->flg=0;
-        $notice->save();
-        break;
+        switch ($tickets) {
+            case 5:
+                $id = Auth::id();
+                $get = new get;
+                $get->badge_id = 1;
+                $get->user_id = $id;
+                $get->get_date = \Carbon\Carbon::today();
+                $get->save();
+                $notice = new Notice;
+                $notice->user_id = $id;
+                $notice->alert_id = 2;
+                $notice->notice = \Carbon\Carbon::today();
+                $notice->flg = 0;
+                $notice->save();
+                break;
+            case 20:
+                $id = Auth::id();
+                $get = new get;
+                $get->badge_id = 2;
+                $get->user_id = $id;
+                $get->get_date = \Carbon\Carbon::today();
+                $get->save();
+                $notice = new Notice;
+                $notice->user_id = $id;
+                $notice->alert_id = 2;
+                $notice->notice = \Carbon\Carbon::today();
+                $notice->flg = 0;
+                $notice->save();
+                break;
+            case 50:
+                $id = Auth::id();
+                $get = new get;
+                $get->badge_id = 3;
+                $get->user_id = $id;
+                $get->get_date = \Carbon\Carbon::today();
+                $get->save();
+                $notice = new Notice;
+                $notice->user_id = $id;
+                $notice->alert_id = 2;
+                $notice->notice = \Carbon\Carbon::today();
+                $notice->flg = 0;
+                $notice->save();
+                break;
+            case 100:
+                $id = Auth::id();
+                $get = new get;
+                $get->badge_id = 4;
+                $get->user_id = $id;
+                $get->get_date = \Carbon\Carbon::today();
+                $get->save();
+                $notice = new Notice;
+                $notice->user_id = $id;
+                $notice->alert_id = 2;
+                $notice->notice = \Carbon\Carbon::today();
+                $notice->flg = 0;
+                $notice->save();
+                break;
+        }
+        return redirect()->action('PersonController@create');
     }
-    return redirect()->action('PersonController@create');
-}
 }
